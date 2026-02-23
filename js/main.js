52570445fd1efcc848f11a3dd0f695b9ce6d3567
Camarederic <camarederic@gmail.com>
2026-02-23 12:10:16 +0300

    added a reference to elements

---
+++ js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Elements
+const chatContainer = document.getElementById("chatContainer");
+const sendBtn = document.getElementById("sendBtn");
+const textBox = document.getElementById("textbox");
