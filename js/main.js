1dc0eb6e782138c6e935e041812363cd21173cde
Camarederic <camarederic@gmail.com>
2026-02-23 12:25:46 +0300

    added a chatbot send message function

--- js/main.js
+++ js/main.js
@@ -5,6 +5,27 @@ const chatContainer = document.getElementById("chatContainer");
 const sendBtn = document.getElementById("sendBtn");
 const textBox = document.getElementById("textbox");
 
+chatbotSendMessage("Hi from Chatbot!");
+
+function chatbotSendMessage(messageText) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("w-50");
+  messageElement.classList.add("float-left");
+  messageElement.classList.add("shadow-sm");
+  messageElement.style.margin = "10px";
+  messageElement.style.padding = "5px";
+
+  messageElement.innerHTML =
+    "<span>Chatbot: </span>" +
+    "<span style=" +
+    "margin-top: 10px; padding: 10px" +
+    ">" +
+    messageText +
+    "</span>";
+
+  chatContainer.appendChild(messageElement);
+}
+
 function sendMessage(messageText) {
   const messageElement = document.createElement("div");
   messageElement.classList.add("w-50");
