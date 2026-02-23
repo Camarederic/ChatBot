1dd52c461d27ccbc1bb1b4c50b04ffbfe4a1df9d
Camarederic <camarederic@gmail.com>
2026-02-23 12:21:45 +0300

    added a send message listener

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@
 const chatContainer = document.getElementById("chatContainer");
 const sendBtn = document.getElementById("sendBtn");
 const textBox = document.getElementById("textbox");
+
+function sendMessage(messageText) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("w-50");
+  messageElement.classList.add("float-right");
+  messageElement.classList.add("shadow-sm");
+  messageElement.style.margin = "10px";
+  messageElement.style.padding = "5px";
+
+  messageElement.innerHTML =
+    "<span>You: </span>" +
+    "<span style=" +
+    "margin-top: 10px; padding: 10px" +
+    ">" +
+    messageText +
+    "</span>";
+
+  chatContainer.appendChild(messageElement);
+}
+
+sendBtn.addEventListener("click", function (e) {
+  let messageText = textBox.value;
+  sendMessage(messageText);
+});
